c256e9da8ad3221ebb96f1f9bb33a98e9c211310
Sebastian Bauer <75776786+yax-lakam-tuun@users.noreply.github.com>
2025-07-30 00:26:18 +0200

    fix warning

--- include/qrcode/micro_qr/symbol_designator.h
+++ include/qrcode/micro_qr/symbol_designator.h
@@ -46,7 +46,7 @@ namespace qrcode::micro_qr
             return designator.error_level;
         }
 
-        [[nodiscard]] friend constexpr auto make_designator(
+        friend constexpr auto make_designator(
             symbol_version version, std::optional<error_correction> error_level) noexcept 
         -> std::optional<symbol_designator>;
 
