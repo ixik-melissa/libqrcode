908081c4dc1bc49ff45ca39aa7a9905a4f902cd7
Sebastian Bauer <75776786+yax-lakam-tuun@users.noreply.github.com>
2025-07-31 03:17:48 +0200

    clang build: comparison operator needs explicit bool declaration otherwise std::sentinel_for fails

--- include/qrcode/code/byte_view.h
+++ include/qrcode/code/byte_view.h
@@ -91,13 +91,13 @@ namespace qrcode::code::detail
         }
 
         [[nodiscard]] friend constexpr auto operator==(
-            byte_view_iterator const& iterator, std::default_sentinel_t const&) noexcept
+            byte_view_iterator const& iterator, std::default_sentinel_t const&) noexcept -> bool
         {
             return !iterator.current_value.has_value();
         }
 
         [[nodiscard]] friend constexpr auto operator==(
-            byte_view_iterator const& a, byte_view_iterator const& b) noexcept
+            byte_view_iterator const& a, byte_view_iterator const& b) noexcept -> bool
         {
             return a.bit_iterator == b.bit_iterator 
                 && a.current_value == b.current_value;
